2a3c8c48c90259702a93d1823566f725d9016fc9
Roman <panichevr@gmail.com>
2026-02-23 10:09:22 +0300

    подготовил проект к развертыванию на render.com.

--- plaster_shop/settings.py
+++ plaster_shop/settings.py
@@ -30,9 +30,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
